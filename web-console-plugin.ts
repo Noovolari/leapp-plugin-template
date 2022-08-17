f8778a3bdc4001037c7f30bd85f53f372333c62f
Alessandro Gaggia <alex@besharp.it>
2022-08-17 16:52:04 +0200

    Update web-console-plugin.ts

--- web-console-plugin.ts
+++ web-console-plugin.ts
@@ -4,50 +4,37 @@ import { PluginLogLevel } from "@noovolari/leapp-core/plugin-sdk/plugin-log-leve
 
 export class WebConsolePlugin extends AwsCredentialsPlugin {
   get actionName(): string {
-    return "Open web console";
+    return "Add a friendly name for your plugin action, eg. My first Plugin";
   }
 
+  /*
+   * Get your icon here: 
+   * https://fontawesome.com/v5/search
+   */
   get actionIcon(): string {
-    return "fa fa-globe";
+    return "Add a font awesome compatible icon tag, e.g. fa fa-globe";
   }
 
+  /*
+   * @params
+   * session       Session            my session object (https://github.com/Noovolari/leapp/blob/master/packages/core/src/models/session.ts)
+   * credentials   Credential-Info    my credentials object (https://github.com/Noovolari/leapp/blob/master/packages/core/src/models/credentials-info.ts)
+   */
   async applySessionAction(session: Session, credentials: any): Promise<void> {
-    this.pluginEnvironment.log("Opening web console for session: " + session.sessionName, PluginLogLevel.info, true);
 
-    const sessionRegion = session.region;
-    const sessionDuration = 3200;
-    const isUSGovCloud = sessionRegion.startsWith("us-gov-");
-    let federationUrl;
-    let consoleHomeURL;
-
-    if (!isUSGovCloud) {
-      federationUrl = "https://signin.aws.amazon.com/federation";
-      consoleHomeURL = `https://${sessionRegion}.console.aws.amazon.com/console/home?region=${sessionRegion}`;
-    } else {
-      federationUrl = "https://signin.amazonaws-us-gov.com/federation";
-      consoleHomeURL = `https://console.amazonaws-us-gov.com/console/home?region=${sessionRegion}`;
-    }
-
-    if (sessionRegion.startsWith("cn-")) {
-      throw new Error("Unsupported Region");
-    }
-
-    this.pluginEnvironment.log("Starting opening Web Console", PluginLogLevel.info, true);
-
-    const sessionStringJSON = {
-      sessionId: credentials.sessionToken.aws_access_key_id,
-      sessionKey: credentials.sessionToken.aws_secret_access_key,
-      sessionToken: credentials.sessionToken.aws_session_token,
-    };
-
-    const queryParametersSigninToken = `?Action=getSigninToken&SessionDuration=${sessionDuration}&Session=${encodeURIComponent(
-      JSON.stringify(sessionStringJSON)
-    )}`;
-
-    const res = await this.pluginEnvironment.fetch(`${federationUrl}${queryParametersSigninToken}`);
-    const response = await res.json();
-
-    const loginURL = `${federationUrl}?Action=login&Issuer=Leapp&Destination=${consoleHomeURL}&SigninToken=${(response as any).SigninToken}`;
-    this.pluginEnvironment.openExternalUrl(loginURL);
+    /* Add your plugin code here... 
+     *
+     * You can also access this.pluginEnvironment which have 3 useful methods
+     *
+     * log("MESSAGE", PluginLogLevel, display?);
+     * e.g. this.pluginEnvironment.log("Starting opening Web Console", PluginLogLevel.info, true);
+     *
+     * fetch("MYURL");
+     * e.g. this.pluginEnvironment.fetch("https://www.google.it");
+     *
+     * openExternalUrl("MYURL");
+     * e.g. this.plugin.pluginEnvironment.openExternalUrl("https://www.google.it");
+     */
+    
   }
 }
